8f70ed842a337b634e171932d08b198608f88b37
nabil0203 <nabilahmed0203@gmail.com>
2024-10-11 15:14:07 +0600

    update

--- week02-practice-01/pyramid.c
+++ week02-practice-01/pyramid.c
@@ -1,27 +1,29 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/T
+
 /*
 
-      * 
-    * * *
-  * * * * *
-* * * * * * *
+   *
+  ***
+ *****
+*******
 
 */
-
 
 #include <stdio.h>
 int main()
 {
     int n;
     scanf("%d", &n);
-    for (int i = 1; i <= n; i++)
+
+    for (int i = 1; i <= n; i++) // row
     {
-        for (int j = 1; j <= n - i; j++)
+        for (int j = 1; j <= n - i; j++) // printing space
         {
             printf(" ");
         }
-        for (int k = 1; k <= i; k++)
+        for (int k = 1; k <= 2 * i - 1; k++) // printing stars
         {
-            printf("* ");
+            printf("*");
         }
         printf("\n");
     }
