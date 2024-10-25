bbebf4371b7b64b9e6b31c1d74e12d3228b25619
nabil0203 <nabilahmed0203@gmail.com>
2024-10-25 22:40:43 +0600

    update

--- mod-11/11.5_lexicographicalOrder.c
+++ mod-11/11.5_lexicographicalOrder.c
@@ -2,6 +2,7 @@
 // array length doesn't matter in most case
 // order of the word matters
 // air <----> airplane ; in this comparison length matters
+//                       because after comparing first 3, there will be nothing to compare
 
 #include <stdio.h>
 int main()
