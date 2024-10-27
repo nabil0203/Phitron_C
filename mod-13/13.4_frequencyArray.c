b7fa104159c5d160f6fd66abf7d5d0c28b2a10ab
nabil0203 <nabilahmed0203@gmail.com>
2024-10-27 21:55:50 +0600

    add

--- mod-13/13.4_frequencyArray.c
+++ mod-13/13.4_frequencyArray.c
@@ -11,16 +11,16 @@ int main()
         scanf("%d", &x[i]);
     }
 
-    int f_arr[100000] = {0}; // full array initalized by 0
+    int f_arr[100000] = {0};                         // full array initalized by 0
 
     for (int i = 0; i < n; i++)
     {
-        f_arr[x[i]] = 1; // the index of f_arr is updated; this index is the same value of x[i]
+        f_arr[x[i]] = 1;                                // the index of f_arr is updated; this index is the same value of x[i]
     }
 
     for (int i = 0; i < 10; i++)
     {
-        printf("%d %d\n", i, f_arr[i]); // showing the index with updated values
+        printf("%d %d\n", i, f_arr[i]);                 // showing the index with updated values
     }
 
     return 0;
