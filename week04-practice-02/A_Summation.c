72b60c01b8aaa5386c4bd05b7ca8d6b9be5dbcfc
nabil0203 <nabilahmed0203@gmail.com>
2024-10-31 22:15:33 +0600

    update

--- week04-practice-02/A_Summation.c
+++ week04-practice-02/A_Summation.c
@@ -1,12 +1,24 @@
 // https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/A
 
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main()
+{
+    int n, sum = 0;
+    scanf("%d", &n);
 
-    
+    int *X = (int *)malloc(n * sizeof(int));
 
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &X[i]);
+        sum = sum + X[i];
+    }
 
+    printf("%d", abs(sum));
 
+    free(X);
 
     return 0;
 }
